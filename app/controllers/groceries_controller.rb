a803def9ae57788643aa8ba5fbabd6ee0768b4e7
Tyler Buchanan <tmailbuchanan@yahoo.com>
2014-04-23 17:23:28 -0700

    finished foundation of app

--- app/controllers/groceries_controller.rb
+++ app/controllers/groceries_controller.rb
@@ -8,6 +8,34 @@ class GroceriesController < ApplicationController
 		@grocery = Grocery.find(params[:id])
 	end
 
+	def new
+		@grocery = Grocery.new
+	end
 
+	def create
+		@grocery = Grocery.create grocery_params
+		redirect_to grocery_path(@grocery)
+	end
+
+	def edit
+		@grocery = Grocery.find(params[:id])
+	end
+
+	def update 
+		@grocery = Grocery.find(params[:id])
+		@grocery.update grocery_params
+		redirect_to grocery_path(@grocery)
+	end
+
+	def destroy
+		Grocery.find(params[:id]).destroy
+		redirect_to root_path
+	end
+
+
+private
+	def grocery_params
+		params.require(:grocery).permit(:title, :description)
+	end
 
 end
